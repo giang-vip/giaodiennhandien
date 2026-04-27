eb008bf6f7c0cbe998345e3f21216e8c30023307
tranhuong0204 <huongtran4002@gmail.com>
2026-04-27 09:23:51 +0700

    something

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,11 +1,11 @@
 class ApiConstants {
   // ================= CHỌN 1 CHẾ ĐỘ =================
-  static const bool useWindowsDesktop = true;
+  static const bool useWindowsDesktop = false;
   static const bool useAndroidEmulator = false;
-  static const bool usePhysicalDevice = false;
+  static const bool usePhysicalDevice = true;
 
   // IP máy tính khi chạy bằng điện thoại thật
-  static const String physicalDeviceIp = '192.168.100.127';
+  static const String physicalDeviceIp = '192.168.1.197';
 
   static const String port = '8081';
 
@@ -22,5 +22,6 @@ class ApiConstants {
   static String get authUrl => 'http://$host:$port/auth';
 
   // AI service
-  static const String aiBaseUrl = 'http://192.168.65.1:8000';
+  static const String aiBaseUrl = 'https://unmethodical-valentina-obscuredly.ngrok-free.dev';
+  // https://unmethodical-valentina-obscuredly.ngrok-free.dev
 }
