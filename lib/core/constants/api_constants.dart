048716847bfcd0c6608c634f9de24839a56a4c7c
thachpham2103 <phamthach2103@gmail.com>
2026-04-28 23:15:17 +0700

    Resolve conflict in api constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -5,21 +5,19 @@ class ApiConstants {
   static const bool usePhysicalDevice = true;
 
   // IP Wi-Fi thật của máy tính (cho điện thoại thật truy cập)
-  static const String physicalDeviceIp = '192.168.1.197';
+  static const String physicalDeviceIp = '192.168.1.4';
 
   static const String port = '8081';
 
   static String get host {
     if (useAndroidEmulator) return '10.0.2.2';
     if (usePhysicalDevice) return physicalDeviceIp;
-    return '192.168.1.197'; // Windows desktop
+    return '127.0.0.1'; // Windows desktop
   }
 
   // ================= FACE APIs =================
   static String get faceRegister => '$baseUrl/face/register';
-  // static String get faceRecognize => '$baseUrl/face/recognize';
-  static String get faceRecognize => '$aiBaseUrl/verify';
-
+  static String get faceRecognize => '$baseUrl/face/recognize';
 
   // ================= BACKEND APIs =================
   static String get baseUrl => 'http://$host:$port/api';
@@ -28,7 +26,5 @@ class ApiConstants {
   static String get authUrl => 'http://$host:$port/auth';
 
   // ================= PYTHON AI SERVICE =================
-  // static const String aiBaseUrl = 'http://192.168.57.109:8000';
-  static const String aiBaseUrl = 'https://unmethodical-valentina-obscuredly.ngrok-free.dev';
-
+  static const String aiBaseUrl = 'http://192.168.57.109:8000';
 }
